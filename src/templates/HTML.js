6043627709ed33b57980ef2ece1591b8dc3880fd
mateusz-lawniczak <admin4pl@gmail.com>
2020-10-17 12:30:03 +0200

    bla bla

---
+++ src/templates/HTML.js
@@ -0,0 +1,51 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+        
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+        <script
+          key="alertify"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          type="text/javascript"
+          aysnc='true'
+        />
+        <script
+          key="fun_javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+        console.log('Happy Coding and Stay Safe!!')
+      `,
+          }}
+        />
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
