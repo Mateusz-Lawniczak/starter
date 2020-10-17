c1173adc572f5ad2a392f7c8dfc40b79218b62c2
mateusz-lawniczak <admin4pl@gmail.com>
2020-10-17 10:48:11 +0200

    test numer kolejny#

---
+++ src/components/adsense.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdSense from 'react-adsense';
+ 
+
+// ads with custom format
+<AdSense.Google
+  client='ca-pub-7852366621322276'
+  slot='7806394673'
+  style={{ width: 500, height: 300, float: 'left' }}
+  format=''
+/>
